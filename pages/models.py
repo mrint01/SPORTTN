b1983e9d3ce41440b844216713ef8fee87b89037
tlilikhaled <khaledtlili9@gmail.com>
2021-12-03 17:11:33 +0100

    newwww

--- pages/models.py
+++ pages/models.py
@@ -5,8 +5,16 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    forget_password_token = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name =  models.CharField(max_length=100)
+    email =   models.EmailField()
+    address = models.CharField(max_length=250,null=True)
+    phone = models.IntegerField(null=True)
+    
+    class Meta:
+          verbose_name_plural = 'Profils'
 
+    def __str__(self):
+      return self.user.username
 
-def __str__(self):
-  return self.user.username
+    
--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('proteine/',views.ProtAction , name="product"),
-     path('proteine/<int:pk>/',views.ProtDetailAction , name="productDetails"),
-]
