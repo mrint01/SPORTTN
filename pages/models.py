4269e0b684c61f83a4eaea9315a86958230fa9e1
tlilikhaled <khaledtlili9@gmail.com>
2021-12-03 16:53:42 +0100

    hhh

--- pages/models.py
+++ pages/models.py
@@ -5,16 +5,8 @@ from django.contrib.auth.models import User
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name =  models.CharField(max_length=100)
-    email =   models.EmailField()
-    address = models.CharField(max_length=250,null=True)
-    phone = models.IntegerField(null=True)
-    
-    class Meta:
-          verbose_name_plural = 'Profils'
+    forget_password_token = models.CharField(max_length=100)
 
-    def __str__(self):
-      return self.user.username
 
-    
+def __str__(self):
+  return self.user.username
